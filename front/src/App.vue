5a094608c31d9be776447b552c4af425774b8c66
Hyuckjin <43224608+hjCode1@users.noreply.github.com>
2020-04-03 13:23:46 +0900

    init

---
+++ front/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="container">
+    <h1>te</h1>
+    <PokemonList :imageUrl="imageUrl" :apiUrl="apiUrl" />
+  </div>
+</template>
+
+<script>
+import PokemonSearch from "@/components/PokemonSearch";
+import PokemonList from "@/components/PokemonList";
+import PokemonDetail from "@/components/PokemonDetail";
+
+export default {
+  data() {
+    return {
+      imageUrl:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/",
+      apiUrl: "https://pokeapi.co/api/v2/pokemon"
+    };
+  },
+  components: {
+    PokemonSearch,
+    PokemonList,
+    PokemonDetail
+  }
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css?family=Acme");
+
+body {
+  margin: 0;
+}
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 10px;
+  width: calc(100% - 20px);
+  min-height: calc(100vh - 20px);
+  background: radial-gradient(#156f99, #0a2e50);
+  font-size: 1rem;
+  font-weight: normal;
+  font-family: "Acme";
+}
+h1 {
+  color: #fff;
+}
+</style>
---
+++ front/src/components/PokemonList.vue
@@ -0,0 +1,76 @@
+<template>
+    <div class="list">
+        <article v-for="(pokemon, index) in pokemons" :key="'poke' + index">
+            <img :src="imageUrl + pokemon.id + '.png'" alt width="96" height="96" />
+            <h3>{{ pokemon.name }}</h3>
+        </article>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['imageUrl', 'apiUrl'],
+    data() {
+        return {
+            pokemons: [],
+            nextUrl: ''
+        };
+    },
+    methods: {
+        fetchData() {
+            let req = new Request(this.apiUrl);
+            fetch(req)
+                .then(res => {
+                    if (res.status === 200) {
+                        return res.json();
+                    }
+                })
+                .then(data => {
+                    this.nextUrl = data.next;
+                    data.results.forEach(pokemon => {
+                        pokemon.id = pokemon.url
+                            .split('/')
+                            .filter(function(part) {
+                                return !!part;
+                                // part 는 https pokeapi api ...
+                                // 여기서 마지막 true값은 숫자임
+                                // 마지막 숫자를 반환
+                            })
+                            .pop();
+                        this.pokemons.push(pokemon);
+                    });
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }
+    },
+    created() {
+        this.fetchData();
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+.list {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
+    grid-gap: 10px;
+    width: 100%;
+    max-width: 510px;
+
+    article {
+        height: 150px;
+        background: #efefef;
+        text-align: center;
+        text-transform: capitalize;
+        border-radius: 5px;
+        cursor: pointer;
+        box-shadow: 0 15px 30px rgba(0, 0, 0, 0.2), 0 10px 10px rgba(0, 0, 0, 0.2);
+
+        h3 {
+            margin: 0;
+        }
+    }
+}
+</style>
